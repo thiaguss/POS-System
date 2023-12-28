c36b4f2c932ce13995270012d2e9d093f8995581
Thiago Santos <tsantos7098@gmail.com>
2023-12-28 07:39:08 -0300

    first commit

---
+++ src/test/java/com/ThiagoSantos/PDV/PdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ThiagoSantos.PDV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
